e0aac6af934fdbeeb86a4520f38424e18e25ac12
Satyajit Koijam <30865861+satyajitkoijam@users.noreply.github.com>
2023-08-07 11:56:12 +0530

    first test

---
+++ main.py
@@ -0,0 +1,10 @@
+import webbrowser
+import time
+
+url = "https://www.youtube.com/watch?v=WFLkOppp_Xo&ab_channel=SatyajitKoijam"
+webbrowser.open(url)
+
+time.sleep(8)
+
+url1 = "https://linktr.ee/satyajitkoijam"
+webbrowser.open(url1)
